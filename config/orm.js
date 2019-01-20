43531f810a5be279f99e77bfa51a06fb590ccd5f
M L <mlecontenyc@gmail.com>
2019-01-19 22:34:11 -0500

    work 80% completed

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,61 @@
-// selectAll()
-// insertOne()
-// updateOne()
+
+
+
+
+// MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+// orm Object encompasses SQL functions.
+var orm = {
+ selectAll: function(table, cb) {
+    var query = "SELECT * FROM " + table + ";";
+    connection.query(query, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  insertOne:function(table, cols, vals, cb) {
+    var query = "INSERT INTO " + table + " (burger_name)  VALUES (?)" 
+    console.log(query, cols, vals);
+    connection.query(query, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // updateOne()
+  update: function(table, objColVals, condition, cb) {
+    console.log(objColVals);
+    //var queryString = "UPDATE " + table + " SET devoured = " +  objColVals.devoured + " WHERE " + condition;
+    var queryString = "UPDATE " + table + " SET devoured = 1 WHERE " + condition
+    connection.query(queryString,[objColVals, condition], function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+//   delete: function(table, condition, cb) {
+//     var queryString = "DELETE FROM " + table;
+//     queryString += " WHERE ";
+//     queryString += condition;
+
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
+
+//       cb(result);
+//     });
+//   }
+ };
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,30 @@
+// ORM is imported so that the file can interact with the burgers_db database.
+var orm = require("../config/orm.js");
+
+var burger = {
+//accessing the selectAll query from orm file
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  //accessing the nsertOne query from orm file
+  create: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
