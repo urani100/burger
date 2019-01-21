9bf3aba9fb72bdcb0c6cc13a20c06da9f1df72b0
M L <mlecontenyc@gmail.com>
2019-01-20 21:32:05 -0500

    addressing heroku error

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,14 @@
 
-// MySQL connection.
+// connection is require to have access to the database
 var connection = require("../config/connection.js");
+
 // orm Object encompasses SQL functions.
 var orm = {
  selectAll: function(table, cb) {
     var query = "SELECT * FROM " + table + ";";
     connection.query(query, function(err, result) {
-      if (err) {
-        throw err;
-      }
+      if (err) throw err;
+      
       cb(result);
     });
   },
@@ -17,9 +17,8 @@ var orm = {
     var query = "INSERT INTO " + table + " (burger_name)  VALUES (?)" 
     console.log(query, cols, vals);
     connection.query(query, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
+      if (err) throw err;
+      
       cb(result);
     });
   },
@@ -29,9 +28,8 @@ var orm = {
     //var queryString = "UPDATE " + table + " SET devoured = " +  objColVals.devoured + " WHERE " + condition;
     var queryString = "UPDATE " + table + " SET devoured = 1 WHERE " + condition
     connection.query(queryString,[objColVals, condition], function(err, result) {
-      if (err) {
-        throw err;
-      }
+      if (err) throw err;
+      
 
       cb(result);
     });
