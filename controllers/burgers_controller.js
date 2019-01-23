2cb9314282eee4f97e7ec48107d0c9b17c849e4f
M L <mlecontenyc@gmail.com>
2019-01-23 15:17:20 -0500

    trouble shooting heroku error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
-var router = express.express();
+var router = express();
+
 //var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
